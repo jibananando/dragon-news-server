ec6bd55be5c37f0ee885f212c4c5290e1400b343
jibananando <jibananando.pramanik@gmail.com>
2022-10-24 04:02:09 +0600

    basic backend server for dragon news

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/caterories.json');
+
+app.get('/', (req, res) => {
+    res.send('News API Running');
+});
+
+app.get('/news-categories', (req, res) => {
+    res.send(categories)
+})
+
+app.listen(port, () => {
+    console.log('Dragon news server running on port', port);
+})
